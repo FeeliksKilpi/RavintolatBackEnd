2ddb1d077dc89979cd7a39000f19bfcf35e651bd
FeeliksKilpi <feelikskilpi@outlook.com>
2020-04-14 13:45:50 +0300

    harjoitustyo

---
+++ src/test/java/com/harjoitustyo/Harjoitustyo/HarjoitustyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harjoitustyo.Harjoitustyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarjoitustyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
